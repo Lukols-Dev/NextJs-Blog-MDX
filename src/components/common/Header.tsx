54d63d17c1f1849af8eee211ad17b78a02f42fa4
Lukols-Dev <lukols.dev@gmail.com>
2023-08-18 00:42:48 +0200

    delete type heading

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,16 +1,12 @@
-type HeadingProps = {
-  id: string;
-} & React.HTMLProps<HTMLHeadingElement>;
-
 export const Heading = {
-  H1: (props: any) => (
-    <h1 {...props} id={props.id} className="text-2xl font-bold">
-      {props.children}
+  H1: ({ id, children }: any) => (
+    <h1 id={id} className="text-2xl font-bold">
+      {children}
     </h1>
   ),
-  H2: (props: any) => (
-    <h2 {...props} id={props.id} className="text-xl font-bold">
-      {props.children}
+  H2: ({ id, children }: any) => (
+    <h2 id={id} className="text-xl font-bold">
+      {children}
     </h2>
   ),
 };
